c4303f960e43f43cd0254640af56b4acf8d9da98
Artan Veliju <artan.veliju@gmail.com>
2022-05-07 04:59:51 +0200

    update main

--- htmlr/htmlr.go
+++ htmlr/htmlr.go
@@ -9,22 +9,35 @@ import (
 )
 
 func main() {
-	// verbose := flag.Bool("v", false, "verbose")
+	var input string
+	if len(os.Args) > 1 {
+		input = os.Args[1]
+		os.Args = remove(os.Args, 1)
+	}
 	output := flag.String("o", "", "output File")
+
 	flag.Parse()
+
 	flag.Usage = func() {
 		fmt.Printf("Usage: %s <template.html> -o <output.html>\n", os.Args[0])
 		flag.PrintDefaults()
 	}
-	var input string
-	if len(os.Args) > 1 {
-		input = os.Args[1]
-	}
 
 	if input == "" {
+		fmt.Println("input template path must be provided")
+		flag.Usage()
+		return
+	}
+
+	if *output == "" {
+		fmt.Println("output template path must be provided")
 		flag.Usage()
 		return
 	}
 
 	htmlr.Resolve(input, *output)
+}
+
+func remove(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
 }
